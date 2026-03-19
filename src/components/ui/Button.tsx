2c6c8469478345695d3738bb4399c8080d507e41
nuazsa <nurazissaputra@gmail.com>
2026-03-20 00:19:41 +0700

    feat(ui): add loading spinner to button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 import clsx from 'clsx';
-import { LucideIcon } from 'lucide-react';
+import { Loader2, LucideIcon } from 'lucide-react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ReactNode;
@@ -59,7 +59,7 @@ export const Button: React.FC<ButtonProps> = ({
     >
       {Icon && <Icon className={sizeStyles[size].icon} />}
       {!isSubmitting && children}
-      {isSubmitting && <span>{submittingText}</span>}
+      {isSubmitting && <span className='flex'><Loader2 className="w-5 h-5 mr-2 animate-spin" /> {submittingText}</span>}
     </button>
   );
 };
